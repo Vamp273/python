ce9a387cd282cf19d00c0a4862e98950526a9db4
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-10-01 21:41:37 +0530

    area

---
+++ TRI.py
@@ -0,0 +1,7 @@
+import math
+a = int(input("Enter 1st no. :"))
+b = int(input("Enter 2st no. :"))
+s1 = math.sqrt(a)
+s2 = math.sqrt(b)
+print("square root of a=", s1)
+print("square root of b=", s2)
