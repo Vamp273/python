26a2dd14f4e3f23bba3ee089fb6d852fa90af44b
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-10-01 20:30:02 +0530

    swapping value of variables

---
+++ new.py
@@ -0,0 +1,14 @@
+x = 5
+y = 6
+print("before swap, Value of x is :" ,x)
+print("before swap, Value of y is :" ,y)
+
+temp = x
+print("Value of temp is :", temp)
+x=y
+print("Value of x is :", x)
+y = temp
+print("Value of y is :", y)
+
+print("After swap, Value of x is :", x)
+print("After swap, Value of y is :", y)
