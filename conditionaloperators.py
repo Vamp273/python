d25893b0e250a87fd6e4bac2a7e60ec6cce14c44
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-10-09 13:15:32 +0530

    Operator

---
+++ conditionaloperators.py
@@ -0,0 +1,13 @@
+a = 5
+b = 10
+if a>b:
+    print("we are in if block")
+else:
+    print("we are in else block")
+
+x = float(input("Enter 1st no. :"))
+y = float(input("Enter 2nd no. :"))
+if x<y:
+    print("YAYY")
+else:
+    print("NAYY")
