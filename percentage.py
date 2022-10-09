08b9b3357f8b0a892581e168c564cae619b9bd3c
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-10-09 14:00:01 +0530

    %que

--- percentage.py
+++ percentage.py
@@ -1,8 +1,10 @@
-#total lecture
-#total attendent
-#calculate%
+#attendance pecentage.
 
 numberOfClassesPerMonth = int(input("Enter total no. of working days ="))
 numberOfClassesAttendent = int(input("Enter how many classes you have attendent ="))
 yourAttendance = (numberOfClassesAttendent/numberOfClassesPerMonth)*100
-print("Your attendance is '%' =", yourAttendance)
+if yourAttendance>75:
+    print("Your attendance is =", yourAttendance, "%. You are elligible for exam")
+else:
+    print("Your attendance is only =", yourAttendance, "%.You are not elligible for exam")
+
