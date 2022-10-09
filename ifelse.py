9b9a4b19d587bb2ed33a4e321fd35a710e961c61
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-10-09 15:08:00 +0530

    if else que

--- ifelse.py
+++ ifelse.py
@@ -2,5 +2,6 @@ length = float(input("Enter length ="))
 breath = float(input("Enter breath ="))
 if length==breath:
     print("It's a Square")
-else:
-    print("It's a Rectangle")
+else:print("It's a Rectangle")
+
+#elif is used when we have more than 1 or 2 condition.
