11dca04b90c5b2c021c6d4c102b72bb1073e0923
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-10-09 16:38:53 +0530

    Encrimented salary

---
+++ program1.py
@@ -0,0 +1,14 @@
+salary = float(input("Enter your annual salary ="))
+yearOfService = float(input("Enter your total no. of years of service in this company ="))
+
+if yearOfService>10:
+    bonus = 0.1*salary
+    print("Your encrimented salary is =", bonus+salary)
+elif 10>yearOfService>6:
+    bonus = 0.8*salary
+    print("Your encrimented salary is =", bonus+salary)
+elif yearOfService<6:
+    bonus = 0.5*salary
+    print("Your encrimented salary is =", bonus+salary)
+else:
+    print("OPPS!!! your salary is not encrimented")
