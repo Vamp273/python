3a2ca07b12bd5c3d693d6bbbd0d9441fbea30765
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-10-09 13:32:15 +0530

    if else operations

---
+++ statementque.py
@@ -0,0 +1,7 @@
+yearOfService = float(input("Enter your total year of service in this company ="))
+currentSalary = float(input("Enter your current salary ="))
+bonus = int(1000)
+if yearOfService>=5:
+    print("Your net bonus is = ", currentSalary+bonus)
+else:
+    print("OOPS!!! No bonus for you")
