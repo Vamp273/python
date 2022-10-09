66eea5cfca0e0a4f68ba635057c8aac4ab07504a
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-10-09 15:30:18 +0530

    too many condition

---
+++ elif.py
@@ -0,0 +1,7 @@
+marks = float(input("Enter your marks ="))
+if 100>=marks>90:
+    print("grade : A")
+elif 90>=marks>80: print("Grade : B")
+elif 80>=marks>70: print("Grade : C")
+elif 70>=marks>60: print("Grade : D")
+elif 60>=marks>00: print("Grade : E")
