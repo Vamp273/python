3f251e6af0d1ae6a24a4d80c0cf7b1933106644c
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-11-10 20:27:49 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,24 @@
+i = 1
+while i<10:
+    print(i)
+    i+=1
+
+i = 1
+while i<100:
+    print(i)
+    if i==50:
+        break
+    i+=1
+
+i = 1
+while i<100:
+    if i==50:
+        i=i+1
+        continue
+    print(i)
+    i+=1
+
+i = 1
+while i<=10:
+    print(i)
+    i+=1
