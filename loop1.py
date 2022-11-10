e4c74eb1904871cbd063d8bdef3e5043c237f44d
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-11-10 20:27:08 +0530

    loop

---
+++ loop1.py
@@ -0,0 +1,12 @@
+# table 
+num = int(input("Enter a number = "))
+i=1
+while i<11:
+    print(i*num)
+    i+=1
+
+num = int(input("Enter a number = "))
+i=1
+while i<11:
+    print(num, "*", i,"=", i*num,)
+    i+=1
