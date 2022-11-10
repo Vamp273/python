191753187246bdfaf07ec290bbb45e253eeec63a
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-11-10 20:34:22 +0530

    que practise

---
+++ fun_subtract.py
@@ -0,0 +1,11 @@
+def sub(num1, num2):
+    if num1>num2:
+        subtract = num1 - num2
+        return(subtract)
+    elif num1<num2:
+        subtract1 = num2 - num1
+        return(subtract1)
+
+x = sub(3,5)
+print(x)
+
