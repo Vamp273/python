8f8ec830bdfa9cf3d1c2fcbab1dfa9c3efb3b0ea
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-11-10 20:38:21 +0530

    que practise

---
+++ forLoop1.py
@@ -0,0 +1,13 @@
+cars = ["Nano", "Ford", "Tata", "BMW", "Thar"]
+for i in cars:
+    if i == "Tata":
+        continue
+    print(i)
+
+# range concept in for loop
+for i in range(0,10):
+    print(i)
+
+#For printing even numbers
+for i in range(0,20,2):   #last 2 is step means kitni step pe chalega ga (1 step ya 2 step saath me lega ki nhi)
+    print(i)
