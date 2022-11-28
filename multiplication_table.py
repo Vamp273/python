3a2a6a220ba515b93ba192b442e8a65535778288
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-11-28 17:14:29 +0530

    Multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,12 @@
+# multiplication table 
+num = int(input("Enter a number = "))
+i=1
+while i<11:
+    print(i*num)
+    i+=1
+
+num = int(input("Enter a number = "))
+i=1
+while i<11:
+    print(num, "X", i,"=", i*num,)
+    i+=1
