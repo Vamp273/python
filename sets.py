6f583f4935e09cf15e5db3f4a51d26241bbec3a4
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-11-28 17:15:10 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,14 @@
+""" sets = store multiple values in single variables
+            unordered
+            unchangable / unmuttable
+            can add or clear
+            dublicate is not allowed
+            hetergenous/non-homogeneous
+
+"""
+
+mySet = {"Car", "Boat", "Train", "Car"}
+print(mySet)
+
+if "Boat" in mySet:
+    print(mySet)
