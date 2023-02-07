1a75532fd4141c5d79ac3f5543687762e727fe6b
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-02-07 10:26:39 +0530

    .

---
+++ Unit5/unit_5_1st.py
@@ -0,0 +1,4 @@
+# this helps us to open any file not just only python file. "r" is for read and "w" is for write and used accordingly whether we
+# want to write the file or read the file.
+f = open("demo1.txt","r")
+print(f.read())
