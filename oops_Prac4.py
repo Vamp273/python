0e2b0b19f0c85d94dc7db5e8994009b4639cc2d0
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-03-23 12:18:13 +0530

    Practise

---
+++ oops_Prac4.py
@@ -0,0 +1,14 @@
+class person:
+    def __init__(self):
+        self.name = "Vaishnavi"
+        self.age = 18
+
+person1 = person()
+person2 = person()
+
+person2.name = "Jyotsana" 
+
+print(person1.name)
+print(person2.name)
+
+
